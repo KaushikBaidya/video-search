3649c50ea1beafdc62f14b415cf84884ba8ad077
KaushikBaidya <47146123+KaushikBaidya@users.noreply.github.com>
2021-09-22 13:52:21 +0600

    search bar component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import SearchBar from './SearchBar'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>MyVideos</h3>
+        <SearchBar />
+      </div>
+    )
+  }
+}
+
+export default App
